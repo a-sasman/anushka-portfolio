e4f9fa61e09661273526c329ed68e163383df396
a-sasman <anushkasasman1@gmail.com>
2026-05-21 11:03:34 +0200

    Initial project upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,21 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import { fileURLToPath } from 'node:url';
+
+import tailwindcss from '@tailwindcss/vite';
+
+const astroPrerenderEntrypoint = fileURLToPath(
+  new URL('./node_modules/astro/dist/entrypoints/prerender.js', import.meta.url)
+);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        'astro/entrypoints/prerender': astroPrerenderEntrypoint
+      }
+    }
+  }
+});
